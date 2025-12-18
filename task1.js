d481a4c697acb00397615df34670d5f7c703239a
shaikh-shifa813 <shaikhruhinmoimran@gmail.com>
2025-12-18 14:12:39 +0530

    add day 1 JS file

---
+++ task1.js
@@ -0,0 +1,32 @@
+//QUESTION(1)
+// Declare variables
+let name = "SHIFA";
+let age = 21;
+let email = "shaikhshifa@gmail.com";
+let isStudent = true;
+// Print variables
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Email:", email);
+console.log("Is Student:", isStudent);
+
+//QUESTION(2)
+Number = Number("25");
+const result = Number + 10;
+console.log(result);
+
+//QUESTION(3)
+let x = 20;
+let y = 30;
+[x,y] = [y,x];
+console.log(x,y);
+
+//QUESTION(4)
+a = 10;
+console.log(a == 8);
+console.log(a != 10);
+console.log(a == 10);
+
+
+
+
