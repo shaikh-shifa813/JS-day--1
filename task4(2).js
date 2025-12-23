495d9c3b3650c10d726df736f2c44bbde40daa8f
shaikh-shifa813 <shaikhruhinmoimran@gmail.com>
2025-12-23 16:50:01 +0530

    Update task4(2).js

--- task4(2).js
+++ task4(2).js
@@ -8,40 +8,36 @@ const userList = document.getElementById("userList");
 
 // function to show list
 function showUsers() {
-
-  // pehle list empty karo
+  
   userList.innerHTML = "";
 
-  // sab users ko loop karo
+  //  loop 
   for (let i = 0; i < users.length; i++) {
 
-    // naya li banao
+    // create new li
     const li = document.createElement("li");
     li.innerText = users[i].name;
 
-    // agar user inactive hai
+    // when user inactive
     if (users[i].active === false) {
       li.classList.add("inactive");
     }
 
-    // button banao
+    // button 
     const btn = document.createElement("button");
     btn.innerText = "Toggle Status";
 
     // button click
     btn.onclick = function () {
-      // active / inactive change karo
+      // active / inactive change 
       users[i].active = !users[i].active;
       showUsers();
     };
-
-    // li ke andar button
+    
     li.appendChild(btn);
 
-    // ul ke andar li
     userList.appendChild(li);
   }
 }
-
-// page load par call
 showUsers();
+
